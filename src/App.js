ee1f08c417c65333b7d9b454d76c709cc5f86c53
Adrian Juansyah Hasan <79002078+T-skyCode@users.noreply.github.com>
2021-08-09 06:17:12 +0700

    Membuat File untuk Komponen

--- src/App.js
+++ src/App.js
@@ -2,9 +2,61 @@ import "./styles.css";
 
 export default function App() {
   return (
-    <div className="App">
-      <h1>Hello CodeSandbox</h1>
-      <h2>Start wwwditing to see some magic happen!</h2>
+    <div className="container">
+      <div className="frame">
+        <section className="header">
+          <button className="header-btn naga">Add</button>
+          <h1 className="header-title">Todo Lists</h1>
+          <button className="header-btn color-red">Clear</button>
+        </section>
+
+        <section className="add">
+          <form className="add-form">
+            <input type="text" className="add-input" />
+            <button className="add-btn">Add</button>
+          </form>
+        </section>
+
+        <section className="todo">
+          <div className="tod">
+            <span className="tod-text">Learning React</span>
+          </div>
+
+          <div className="tod">
+            <span className="tod-text">Learning React</span>
+          </div>
+
+          <div className="tod">
+            <span className="tod-text">Learning React</span>
+          </div>
+
+          <div className="tod">
+            <span className="tod-text">Learning React</span>
+          </div>
+
+          <div className="tod">
+            <span className="tod-text">Learning React</span>
+          </div>
+          <div className="tod">
+            <span className="tod-text">Learning React</span>
+          </div>
+          <div className="tod">
+            <span className="tod-text">Learning React</span>
+          </div>
+
+          <div className="tod">
+            <span className="tod-text">Learning React</span>
+          </div>
+
+          <div className="tod">
+            <span className="tod-text">Learning React</span>
+          </div>
+
+          <div className="tod">
+            <span className="tod-text">Learning React</span>
+          </div>
+        </section>
+      </div>
     </div>
   );
 }
